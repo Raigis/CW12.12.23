da7be0a4d5db7e4409224831397c6b31e091b2aa
Raigis <alerish015@gmail.com>
2023-12-13 02:54:15 +0300

    Finish Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using static Promt;
+using static Print;
+
+Console.WriteLine($"Эта программа выведет на экран все строки с длиною меньше или равной трём.");
+string[] array = Enter{};
+PrintArray(array);
