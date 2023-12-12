a248162b21bf8d1647ff232f9521bbcb412243c9
Raigis <alerish015@gmail.com>
2023-12-13 02:14:37 +0300

    Add Promt.cs

---
+++ Promt.cs
@@ -0,0 +1,20 @@
+public static class Promt{
+    public static string[] Enter(){
+        string[] array = [];
+        string? word;
+        bool isEmpty = true;
+        while (isEmpty){
+            Console.WriteLine($"Введите любую строку. Если желаете остановиться, введите пустую строку.\nВ случае, когда ни одна строка не была введена будет использоваться набор строк по умолчанию.");
+            word = Console.ReadLine();
+            if (word == null) {
+                if (array.Length == 0) {
+                    string[] array_temp = {"one","two","three","four"};
+                    return array_temp;
+                }
+                isEmpty = false;
+            }
+            ArrayAppend(array, word);
+        }
+        return array;
+    }
+}
